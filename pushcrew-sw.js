9b2032c38148904acef2810d382e7d9535b2f6a2
aditi-malhotra <59558853+aditi-malhotra@users.noreply.github.com>
2020-01-08 15:16:31 +0530

    Update pushcrew-sw.js

--- pushcrew-sw.js
+++ pushcrew-sw.js
@@ -1,2 +1,2 @@
 var version = 6;
-importScripts('https://cdn.pushcrew.com/sw/672f02146cf6e0dda79dc1a45ae83cb6.js');
+importScripts('https://cdn.pushcrew.com/sw/e6ad27220e3a760a872fe277a8e53b45.js');
